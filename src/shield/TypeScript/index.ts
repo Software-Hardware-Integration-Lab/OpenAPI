9e8746eac270c9f8707f9b08e014857e01400e42
Elliot Huffman <ehuffman@elliot-labs.com>
2025-03-29 21:23:10 -0400

    Create SDK Clients
    Create code, license, build process, readme, and structure.
    This process is able to build SDKs based on the OpenAPI specs in the specs folder.
    The SDK folder needs to be generated locally as it is generated content.
    The project is structured as a mono repo with a shared TS Config.

---
+++ src/shield/TypeScript/index.ts
@@ -0,0 +1,33 @@
+import type { TokenCredential } from "@azure/core-auth";
+import { AzureIdentityAuthenticationProvider } from "@microsoft/kiota-authentication-azure";
+import { FetchRequestAdapter } from "@microsoft/kiota-http-fetchlibrary";
+import { createShieldClient } from "./sdk/shieldClient.js";
+import { assert, assertGuardEquals, tags } from 'typia';
+
+/**
+ * Function that initializes the SHIELD SDK.
+ * @param credential Configured authentication session from Entra ID.
+ * @returns Configured API client that is able to make requests against the specified SHIELD instance.
+ */
+export function shieldClientFactory(credential: TokenCredential, baseUrl: string & tags.Format<'hostname'>) {
+    // #region Input Validation
+    assert(credential);
+
+    assertGuardEquals(baseUrl);
+    // #endregion Input Validation
+
+    /** List of hosts that are allowed when making API calls, this is used to prevent token leaks to threat actors. */
+    const allowedHostList = new Set([baseUrl]);
+
+    /** Authentication system that will be used to configure the SDK client. */
+    const authProvider = new AzureIdentityAuthenticationProvider(credential, void 0, void 0, allowedHostList);
+
+    /** Instance of the SHIELD SDK client initialization configuration. */
+    const shieldAdapter = new FetchRequestAdapter(authProvider);
+
+    // Set the base URL to be what is provided, since the host name is unique every deployment
+    shieldAdapter.baseUrl = `https://${ baseUrl }`;
+
+    /** Instance of the API client that can be used for SHIELD access. */
+    return createShieldClient(shieldAdapter);
+}
